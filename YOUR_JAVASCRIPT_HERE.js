cd2449968bd1f0274d89a46ac9d247e138639b8d
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-03-23 12:20:08 +0100

    resting

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,3 +1,27 @@
 const hero = {
-    
+    name: 'Kevin',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'slashing',
+        damage: 2,
+    },
 }
+
+function rest(Object) {
+    if(Object.health !== 10){
+    Object.health = 10
+ } else return alert('Your health is already full!')
+ return Object  }
+
+function pickUpItem(Object, weapon) {
+    Object.inventory.push(weapon)
+}
+
+function equipWeapon() {
+
+}
+
+rest(hero)
+console.log(hero.health)
